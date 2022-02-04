fce05eb157518de244d47dc90279290233fd99f4
JoaoCabrera <joaocabrerati@gmail.com>
2022-02-04 14:08:48 -0300

    Tenta requisito 5 denovo

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -2,6 +2,7 @@ from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
 import json
+import xmltodict
 
 
 def read_CSV(path):
@@ -22,12 +23,22 @@ def read_JSON(path):
     return inventories
 
 
+def read_XML(path):
+    with open(path) as file:
+        read_file = file.read()
+        convert_to_dict = xmltodict.parse(read_file)
+        inventories = convert_to_dict["dataset"]["record"]
+    return inventories
+
+
 def read(path):
-    type_file = path.split('.')
+    type_file = path.split(".")
     if type_file[1] == "csv":
         return read_CSV(path)
     elif type_file[1] == "json":
         return read_JSON(path)
+    elif type_file[1] == "xml":
+        return read_XML(path)
 
 
 class Inventory:
