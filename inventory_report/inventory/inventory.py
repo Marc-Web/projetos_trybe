317fffa70bbb909a6edc650a679a9d0753b5faaa
JoaoCabrera <joaocabrerati@gmail.com>
2022-02-04 09:57:08 -0300

    Adiciona requisito 4

--- inventory_report/inventory/inventory.py
+++ inventory_report/inventory/inventory.py
@@ -1,15 +1,33 @@
 from inventory_report.reports.simple_report import SimpleReport
 from inventory_report.reports.complete_report import CompleteReport
 import csv
+import json
 
 
-def read(path):
+def read_CSV(path):
     with open(path) as file:
         inventory_reader = csv.DictReader(file, delimiter=",", quotechar='"')
         inventories = []
         for inv in inventory_reader:
             inventories.append(inv)
-        return inventories
+    return inventories
+
+
+def read_JSON(path):
+    with open(path) as file:
+        inventory_reader = json.load(file)
+        inventories = []
+        for inv in inventory_reader:
+            inventories.append(inv)
+    return inventories
+
+
+def read(path):
+    type_file = path.split('.')
+    if type_file[1] == "csv":
+        return read_CSV(path)
+    elif type_file[1] == "json":
+        return read_JSON(path)
 
 
 class Inventory:
